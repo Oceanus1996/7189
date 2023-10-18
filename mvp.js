956c1d26b30d227583bbab417fd5ee73f5744e3d
Oceanus1996 <rrm_accounting@126.com>
2023-10-18 14:10:58 +1000

    1018加了旁白，

--- mvp.js
+++ mvp.js
@@ -1,4 +1,3 @@
-
 // 初始化地图
 var map = L.map('map').setView([-3.56518,143.619995], 5); // 使用Wewak, New Guinea作为初始中心
 
@@ -106,7 +105,7 @@ function handleMarkerClick(url,image,marker){
     });
 }
 
-//保存数据并发送给experience
+// 保存数据并发送给experience
 function sendUrlsToServer() {
     $.ajax({
         url: "storageData.php",
@@ -126,57 +125,132 @@ function sendUrlsToServer() {
         }
     });
 }
-//定时保存，每3秒
+// 定时保存，每3秒
 setInterval(sendUrlsToServer, 3*1000);
 
 //加标记点的文档
 function addPointToMap(lat, lon,recordValue) {
     var marker =L.marker([lat, lon]).addTo(map);
-    marker.isGrab = false;
+    marker.setIcon(sourceicon)
+    var originalSize = [40, 40]; // 假设这是原始大小
+    var enlargedSize = [60, 60]; // 放大后的大小
+    var dragonicon ="icon\\dragonicon.png";
+    var treasureicon = "icon\\treasure.png"
+    marker.isGrab = false
+    marker.on('mouseover', function() {
+        if(marker.isGrab == false){
+            var icon = L.icon({
+                iconUrl: dragonicon,
+                iconSize: enlargedSize,
+                iconAnchor: [22, 94],
+            });
+        }else{
+            var icon = L.icon({
+                iconUrl: treasureicon,
+                iconSize: enlargedSize,
+                iconAnchor: [22, 94],
+            });
+        }
+        this.setIcon(icon)
+    }).on('mouseout', function() {
+        if(marker.isGrab == false){
+            var icon = L.icon({
+                iconUrl: dragonicon,
+                iconSize: originalSize,
+                iconAnchor: [22, 94],
+            });
+        }else{
+            var icon = L.icon({
+                iconUrl: treasureicon,
+                iconSize: originalSize,
+                iconAnchor: [22, 94],
+            });
+        }
+        this.setIcon(icon);
+    });
+    
     var supportClick = false;
     markerArray.push(marker);
     //数据存储到marker对象中
     marker.recordData =recordValue.URL;//url储存了
+    marker.marker=recordValue
     marker.image = recordValue['Primary image'];
     //点击时执行括号(事件监听器)
     marker.on("click",function(e){
         //监听点击
         //询问框，询问是否挖掘
-        showConfirmationPopup(e.target,function(result){
+        showConfirmationPopup(e.target,marker,function(result){
             supportClick = result;
+            marker.isGrab = result;
             if(supportClick){
                 openModal.call(marker);
                 handleMarkerClick(this.recordData,this.image,marker);
+                //移除旧的事件处理函数
+                $("#navabutton").off("click", showNextNarration);
+                // 绑定新的事件处理函数
+                $("#navabutton").on("click", congrateNarration);
+                // 显示按钮（确保它是可见的）
+                $("#navabutton").show();
+
                 marker.setIcon(customIcon);
             }
         }.bind(this));             
     });
 }
-//询问框->侧边框
-function showConfirmationPopup(clickedMarker, callback) {
-    var popupContent = `
-        <p>Dig now?</p>
-        <button id="startGrab">Sure</button>
-        <button id="noGrab">Not yet</button>
-    `;
-    clickedMarker.bindPopup(popupContent).openPopup();
-    document.getElementById("startGrab").addEventListener('click',function(){
 
-        if (typeof callback === "function") {
-            callback(true);
-        }
-        clickedMarker.closePopup();
-        clickedMarker.unbindPopup();
-    });
-    document.getElementById("noGrab").addEventListener('click',function(){
-        clickedMarker.closePopup();
-        if (typeof callback === "function") {
-            callback(false);
-        }
-    });
-    document.getElementById('confirmationSidebar').classList.remove('hidden');
+
+    function showConfirmationPopup(clickedMarker, marker,callback) {
+            // 先移除旧的侧边栏
+    const oldSidebar = document.getElementById('sidebar');
+    if (oldSidebar) {
+        oldSidebar.parentNode.removeChild(oldSidebar);
+    } 
+
+        const sidebarHtml = document.createElement("div");
+        sidebarHtml.id = "sidebar";
+        sidebarHtml.classList.add('sidebar');
+        var Date = marker.marker.Date;
+        var caption = marker.marker['Title'];
+        var place = marker.marker['Place'];
+        var keywords = marker.marker['Keywords'];
+        var images = marker.marker['Primary image'];
+        console.log(place);
+        sidebarHtml.innerHTML = `
+            <img src="icon\\dragon2.png" alt="No picture" width="250">
+            <p>Memory:${caption}<p>
+            <button id="closeIcon" style="position: absolute; top: 10px; right: 10px;">X</button>
+            <p>Locaion: ${place}<br>Date: ${Date}<br>keywords: ${keywords}<br></p>
+            <button id="startDig">Dig</button>
+            <button id="closeSidebar">Cancel</button>
+        `;
+        // 添加新的侧边栏到body
+        document.getElementById("map").appendChild(sidebarHtml);
+        
+        const audio = document.getElementById("sidebarAudio");
+        // 播放音效
+        audio.play();
     
-}
+        document.getElementById("startDig").addEventListener('click', function() {
+            if (typeof callback === "function") {
+                callback(true);
+            }
+            clickedMarker.closePopup();
+            clickedMarker.unbindPopup();
+            // 停止音效并重置播放位置
+            audio.pause();
+            audio.currentTime = 0;
+        });
+    
+        document.getElementById('closeSidebar').addEventListener('click', function() {
+            const sidebar = document.getElementById('sidebar');
+            document.getElementById("map").removeChild(sidebar);
+            audio.pause();
+            audio.currentTime = 0;
+        });
+    } 
+
+
+    
 //侧边框关闭
 function closeConfirmationSidebar() {
     document.getElementById('confirmationSidebar').classList.add('hidden');
@@ -195,10 +269,8 @@ function iterateRecords(data) {
             var Longitude = recordValue.Longitude + (Math.random() * 0.2 - 0.1)
             if(latitude && Longitude){
                 addPointToMap(latitude,Longitude,recordValue)
-        
             }
-            
-            }
+        }
     }
     )
 }
@@ -256,8 +328,8 @@ function showCurrent(){
 
 //锚点格式,需要调整*
 var customIcon = L.icon({
-    iconUrl: 'icon.png',    // 图标的URL
-    iconSize: [60, 50],       // 图标的大小
+    iconUrl: 'icon\\treasure.png',    // 图标的URL
+    iconSize: [50, 50],       // 图标的大小
     iconAnchor: [22, 94],     // 图标的锚点位置，使图标的这个点正好对齐于地图上的对应点
     popupAnchor: [-3, -76]    // 如果你想为这个标记添加一个popup，这个是popup的锚点位置
 });
@@ -268,21 +340,54 @@ var userIcon = L.icon({
     iconSize: [60, 60],       // 图标的大小
     iconAnchor: [22, 94],     // 图标的锚点位置，使图标的这个点正好对齐于地图上的对应点
 })
+//最初的icon
+var sourceicon = L.icon({
+    iconUrl:  'icon\\dragonicon.png',  // 图片的路径
+    iconSize: [60, 50], // 图标的大小
+    iconAnchor: [22, 94], // 图标的锚点，确保图标在正确的地方显示
+    popupAnchor: [-3, -76] // popup的锚点
+});
 
 //旁白
-let currentNarration = 1; // 当前显示的旁白
+let currentNarration = 0; // 当前显示的旁白
+let currentCongration = 0; // 当前显示的祝贺旁白
 
 function showNextNarration() {
     // 隐藏当前的旁白
     $(`#narration-${currentNarration}`).addClass('hidden');
-
     currentNarration++;
 
     // 如果还有旁白需要显示
-    if (currentNarration <= 6) {
+    if (currentNarration <= 5) {
         $(`#narration-${currentNarration}`).removeClass('hidden');
     } else {
         // 所有旁白都显示完后，隐藏按钮
         $('#narration-container button').hide();
     }
+}
+
+function congrateNarration() {
+    //绑定事件
+    
+    $('#narration-container button').show();
+    // 隐藏当前的旁白
+    $(`#narration-${currentNarration}`).addClass('hidden');
+    // 隐藏之前显示的祝贺旁白
+    $(`#congration-${currentCongration}`).addClass('hidden');
+    currentCongration++;
+    
+    if (currentCongration <= 4) {
+        console.log("congrateNarration",currentCongration)
+        // 如果还有祝贺旁白需要显示
+        $(`#congration-${currentCongration}`).removeClass('hidden');
+    } else {
+        // 所有祝贺旁白都显示完后，隐藏按钮
+        $('#narration-container button').hide();
+        currentCongration=0;
+    }
+}
+
+function playAudio() {
+    let audio = document.getElementById("myAudio");
+    audio.play();
 }
